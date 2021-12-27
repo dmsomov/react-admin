603b218cc5066441e8cc88165e9ab4cfcfd4c085
dmsomov <somovdmitriy@icloud.com>
2021-12-27 23:01:15 +0300

    feat: add basic page layout

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Admin, Resource } from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import PostList from './components/PostList'
+import PostCreate from './components/PostCreate'
+import PostEdit from './components/PostEdit'
+import UserList from './components/UserList'
+import UserEdit from './components/UserEdit'
+import UserCreate from './components/UserCreate'
+
+function App() {
+  return <Admin dataProvider={restProvider('http://localhost:3000')}>
+    <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit}/>
+    <Resource name='users' list={UserList} create={UserCreate} edit={UserEdit}/>
+  </Admin>
+}
+
+export default App;
